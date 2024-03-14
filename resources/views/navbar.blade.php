f26f8041d0c1bb48ed3b4ab317d02316f284034e
vickymaulana <vickymaulanna@gmail.com>
2024-03-14 10:04:32 +0700

    Navigation Bar

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">LLDIKTI 2</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto" style="text-align: center;"> <!-- Using inline style -->
+            <li class="nav-item active">
+                <a class="nav-link text-primary" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-primary" href="#">Berita</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-primary" href="#">Info</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-primary" href="#">Layanan</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-primary" href="#">Informasi Publik</a>
+            </li>
+        </ul>
+    </div>
+</nav>
