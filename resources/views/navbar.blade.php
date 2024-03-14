90ab1bf44a845d9bb4a31e03208279601cd63a9c
vickymaulana <vickymaulanna@gmail.com>
2024-03-14 13:33:43 +0700

    styled update

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,3 +1,5 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 <nav class="navbar navbar-expand-lg navbar-transparent">
     <a class="navbar-brand" href="#">LLDIKTI 2</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -27,3 +29,32 @@
         </form>
     </div>
 </nav>
+
+<style>
+    .btn-outline-success:hover {
+        background-color: blue;
+        color: white;
+    }
+
+    .btn-outline-success {
+        color: blue;
+    }
+
+    .navbar-transparent {
+        background-color: transparent;
+    }
+
+    .btn-outline-success {
+        color: blue;
+    }
+
+    .btn-outline-success:hover {
+        background-color: blue;
+        color: white;
+    }
+
+    .navbar-transparent {
+        background-color: transparent;
+    }
+
+</style>
