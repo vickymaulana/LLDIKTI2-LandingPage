64210b3e2c9e58e14619547a4f9896456cff172d
vickymaulana <vickymaulanna@gmail.com>
2024-03-14 10:43:05 +0700

    Menambah Pencarian

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,10 +1,10 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-transparent">
     <a class="navbar-brand" href="#">LLDIKTI 2</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ml-auto" style="text-align: center;"> <!-- Using inline style -->
+        <ul class="navbar-nav mx-auto" style="text-align: center;">
             <li class="nav-item active">
                 <a class="nav-link text-primary" href="#">Home</a>
             </li>
@@ -21,5 +21,9 @@
                 <a class="nav-link text-primary" href="#">Informasi Publik</a>
             </li>
         </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Cari" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Cari</button>
+        </form>
     </div>
 </nav>
