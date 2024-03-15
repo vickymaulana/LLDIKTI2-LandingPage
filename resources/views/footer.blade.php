54d81da2007e846c244f438ae03d6369eddd3996
TriyanaSgi <triyanasugiyarti@gal.com>
2024-03-15 09:01:27 +0700

    ubah maps

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -20,7 +20,7 @@
         <div class="row">
             <div class="col">
                 <!-- Google Maps Embed -->
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.458244923354!2d106.80228981525302!3d-6.1869329955027355!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6a416c9c76a025%3A0x81397204e1aaeddf!2sMonumen%20Nasional!5e0!3m2!1sen!2sid!4v1649161730766!5m2!1sen!2sid" width="300" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3984.5234901032986!2d104.72437707374056!3d-2.9521276397134186!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e3b74498f25a843%3A0xc6061fa55693d63a!2sLembaga%20Layanan%20Pendidikan%20Tinggi%20(L2Dikti)%20Wilayah%20II!5e0!3m2!1sid!2sid!4v1710467309903!5m2!1sid!2sid" width="300" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
             </div>
         </div>
         <div class="row">
