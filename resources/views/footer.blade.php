324e87fc2a7c383f498c7391042031832bc49b72
haikalalbanna <haikalalbanna2313@gmail.com>
2024-03-18 13:39:30 +0700

    menghubungkan icon ke link

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,44 +1,41 @@
-  
-  <div class="container-footer" style="background-color: rgba(3, 65, 158, 0.03);">
-        <div class="row py-5" >
-            <div class="col-md-4" >
-                <img class="image-logo" src="/img/logoLLDikti2.png" width="220px"> <br>
-                <a href="#" style="margin-left: 25px; font-size:20px; color: #03419E">Beranda</a> <br>
-                <a href="#" style="margin-left: 25px; font-size:20px; color: #03419E">Berita</a> <br>
-                <a href="#" style="margin-left: 25px; font-size:20px; color: #03419E">Info</a> <br>
-                <a href="#" style="margin-left: 25px; font-size:20px; color: #03419E">Layanan</a> <br>
-                <a href="#" style="margin-left: 25px; font-size:20px; color: #03419E">Informasi Publik</a> 
-            </div>
+<div class="container-footer" style="background-color: rgba(3, 65, 158, 0.03);">
+    <div class="row py-5">
+        <div class="col-md-4">
+            <img class="image-logo" src="/img/logoLLDikti2.png" width="220px"> <br>
+            <a href="#" style="margin-left: 25px; font-size:20px; color: #03419E">Beranda</a> <br>
+            <a href="#" style="margin-left: 25px; font-size:20px; color: #03419E">Berita</a> <br>
+            <a href="#" style="margin-left: 25px; font-size:20px; color: #03419E">Info</a> <br>
+            <a href="#" style="margin-left: 25px; font-size:20px; color: #03419E">Layanan</a> <br>
+            <a href="#" style="margin-left: 25px; font-size:20px; color: #03419E">Informasi Publik</a> 
+        </div>
 
-            <div class="col-md-4">
-                <!-- Google Maps Embed --> 
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3984.5234901032986!2d104.72437707374056!3d-2.9521276397134186!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e3b74498f25a843%3A0xc6061fa55693d63a!2sLembaga%20Layanan%20Pendidikan%20Tinggi%20(L2Dikti)%20Wilayah%20II!5e0!3m2!1sid!2sid!4v1710467309903!5m2!1sid!2sid" 
-                width="490" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-            </div>
+        <div class="col-md-4">
+            <!-- Google Maps Embed --> 
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3984.5234901032986!2d104.72437707374056!3d-2.9521276397134186!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e3b74498f25a843%3A0xc6061fa55693d63a!2sLembaga%20Layanan%20Pendidikan%20Tinggi%20(L2Dikti)%20Wilayah%20II!5e0!3m2!1sid!2sid!4v1710467309903!5m2!1sid!2sid" 
+            width="490" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
 
-            <div class="col-md-4" style="color: #03419E; font-size:17px;">
-                <p style="margin-left: 25px"><i class="fas fa-phone" ></i> (0711) 410423</p>
-                <p style="margin-left: 25px"><i class="fas fa-map-marker-alt"></i> Jalan Srijaya No. 883</p>
-                <p style="margin-left: 25px"><i class="fas fa-envelope"></i> admin@lldikti2.id</p>
-                <p style="margin-left: 25px"><i class="fas fa-clock"></i> Senin - Kamis : 07.30 - 16.00 WIB</p>
-                <p style="margin-left: 43px">Jumat &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; : 07.30 - 16.30 WIB </p>
+        <div class="col-md-4" style="color: #03419E; font-size:17px;">
+            <p style="margin-left: 25px"><i class="fas fa-phone"></i> (0711) 410423</p>
+            <p style="margin-left: 25px"><i class="fas fa-map-marker-alt"></i> Jalan Srijaya No. 883</p>
+            <p style="margin-left: 25px"><i class="fas fa-envelope"></i> admin@lldikti2.id</p>
+            <p style="margin-left: 25px"><i class="fas fa-clock"></i> Senin - Kamis : 07.30 - 16.00 WIB</p>
+            <p style="margin-left: 43px">Jumat &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; : 07.30 - 16.30 WIB </p>
 
-                <p style="margin-left: 110px; margin-top: 50px; font-size:28px">
-                    <i class="fab fa-instagram"></i>&nbsp;&nbsp;&nbsp; 
-                    <i class="fab fa-twitter"></i>&nbsp;&nbsp;&nbsp; 
-                    <i class="fab fa-tiktok"></i>&nbsp;&nbsp;&nbsp; 
-                    <i class="fab fa-facebook"></i>&nbsp;&nbsp;&nbsp; 
-                    <i class="fab fa-youtube"></i>&nbsp;&nbsp;&nbsp;
-                </p>
-            </div>
+            <!-- Tautan media sosial -->
+            <p style="margin-left: 110px; margin-top: 50px; font-size:28px">
+                <a href="https://www.instagram.com/lldiktii2/" target="_blank"><i class="fab fa-instagram"></i></a>&nbsp;&nbsp;&nbsp; 
+                <a href="https://twitter.com/LLDIKTI2" target="_blank"><i class="fab fa-twitter"></i></a>&nbsp;&nbsp;&nbsp; 
+                <a href="https://www.tiktok.com/@lldikti2" target="_blank"><i class="fab fa-tiktok"></i></a>&nbsp;&nbsp;&nbsp; 
+                <a href="https://www.facebook.com/lldikti2/" target="_blank"><i class="fab fa-facebook"></i></a>&nbsp;&nbsp;&nbsp; 
+                <a href="https://www.youtube.com/channel/UCuiuUQJ3ATDDp9zI23aH_zQ" target="_blank"><i class="fab fa-youtube"></i></a>&nbsp;&nbsp;&nbsp;
+            </p>
         </div>
     </div>
-        
-        </div>
-        <div class="row" style="background-color: #03419E;">
-            <div  class="col">
-                <p class="text-center" style="color: white;">&copy; <?php echo date("Y"); ?> LLDIKTI Wilayah 2 - All Rights Reserved.</p>
-            </div>
-        </div>
-    </div>
+</div>
 
+<div class="row" style="background-color: #03419E;">
+    <div  class="col">
+        <p class="text-center" style="color: white;">&copy; <?php echo date("Y"); ?> LLDIKTI Wilayah 2 - All Rights Reserved.</p>
+    </div>
+</div>
