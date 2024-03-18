6d2c447a875e1fc8bc5300cd77ca4852681ac8b5
haikalalbanna <haikalalbanna2313@gmail.com>
2024-03-18 09:14:28 +0700

    statistik

---
+++ resources/views/statistik.blade.php
@@ -0,0 +1,424 @@
+<style>
+.data-statistik-perguruan-tinggi,
+.data-statistik-perguruan-tinggi * {
+  box-sizing: border-box;
+}
+.data-statistik-perguruan-tinggi {
+  color: #000000;
+  text-align: center;
+  font-family: "Outfit-Bold", sans-serif;
+  font-size: 40px;
+  line-height: 66px;
+  font-weight: 700;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
+
+<div class="data-statistik-perguruan-tinggi">
+  Data Statistik Perguruan Tinggi
+</div>
+
+<style>
+.frame-36932,
+.frame-36932 * {
+  box-sizing: border-box;
+}
+.frame-36932 {
+  background: rgba(3, 65, 158, 0.01);
+  border-radius: 50px;
+  height: 322px;
+  position: relative;
+  overflow: hidden;
+}
+.rectangle-176 {
+  width: 175px;
+  height: 173px;
+  position: absolute;
+  left: 84px;
+  top: 49.5px;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  object-fit: cover;
+}
+.image-22 {
+  width: 194px;
+  height: 194px;
+  position: absolute;
+  left: 369px;
+  top: 39px;
+  object-fit: cover;
+}
+.image-23 {
+  width: 157px;
+  height: 157px;
+  position: absolute;
+  left: 673px;
+  top: 57.5px;
+  object-fit: cover;
+}
+.image-24 {
+  width: 155px;
+  height: 155px;
+  position: absolute;
+  left: 940px;
+  top: 58.5px;
+  object-fit: cover;
+}
+.jumlah-perguruan-tinggi {
+  color: #000000;
+  text-align: center;
+  font-family: "Outfit-Bold", sans-serif;
+  font-size: 20px;
+  line-height: 66px;
+  font-weight: 700;
+  position: absolute;
+  left: 56px;
+  top: 256px;
+  width: 230px;
+}
+.rectangle-247 {
+  border-radius: 0px;
+  width: 243px;
+  height: 72px;
+  position: absolute;
+  left: 50px;
+  top: 242px;
+  overflow: visible;
+}
+.rectangle-248 {
+  border-radius: 0px;
+  width: 243px;
+  height: 72px;
+  position: absolute;
+  left: 345px;
+  top: 242px;
+  overflow: visible;
+}
+._642-919 {
+  color: #000000;
+  text-align: center;
+  font-family: "Outfit-Bold", sans-serif;
+  font-size: 24px;
+  line-height: 66px;
+  font-weight: 700;
+  position: absolute;
+  left: 419px;
+  top: 231px;
+  width: 92px;
+  height: 44px;
+}
+.jumlah-mahasiswa {
+  color: #000000;
+  text-align: center;
+  font-family: "Outfit-Bold", sans-serif;
+  font-size: 20px;
+  line-height: 66px;
+  font-weight: 700;
+  position: absolute;
+  left: 379px;
+  top: 258px;
+}
+.rectangle-249 {
+  border-radius: 0px;
+  width: 243px;
+  height: 72px;
+  position: absolute;
+  left: 630px;
+  top: 242px;
+  overflow: visible;
+}
+._1-987 {
+  color: #000000;
+  text-align: center;
+  font-family: "Outfit-Bold", sans-serif;
+  font-size: 24px;
+  line-height: 66px;
+  font-weight: 700;
+  position: absolute;
+  left: 715px;
+  top: 231px;
+  width: 73px;
+  height: 44px;
+}
+.jumlah-program-studi {
+  color: #000000;
+  text-align: center;
+  font-family: "Outfit-Bold", sans-serif;
+  font-size: 20px;
+  line-height: 66px;
+  font-weight: 700;
+  position: absolute;
+  left: 649px;
+  top: 258px;
+}
+.rectangle-250 {
+  border-radius: 0px;
+  width: 243px;
+  height: 72px;
+  position: absolute;
+  left: 896px;
+  top: 242px;
+  overflow: visible;
+}
+._25-119 {
+  color: #000000;
+  text-align: center;
+  font-family: "Outfit-Bold", sans-serif;
+  font-size: 24px;
+  line-height: 66px;
+  font-weight: 700;
+  position: absolute;
+  left: 981px;
+  top: 231px;
+  width: 73px;
+  height: 44px;
+}
+.jumlah-dosen-tetap {
+  color: #000000;
+  text-align: center;
+  font-family: "Outfit-Bold", sans-serif;
+  font-size: 20px;
+  line-height: 66px;
+  font-weight: 700;
+  position: absolute;
+  left: 925px;
+  top: 261px;
+}
+._270 {
+  color: #000000;
+  text-align: center;
+  font-family: "Outfit-Bold", sans-serif;
+  font-size: 24px;
+  line-height: 66px;
+  font-weight: 700;
+  position: absolute;
+  left: 135px;
+  top: 233px;
+  width: 73px;
+  height: 44px;
+}
+</style>
+
+<div class="frame-36932">
+  <img class="rectangle-176" src="rectangle-1760.png" />
+  <img class="image-22" src="image-220.png" />
+  <img class="image-23" src="image-230.png" />
+  <img class="image-24" src="image-240.png" />
+  <div class="jumlah-perguruan-tinggi">Jumlah Perguruan Tinggi</div>
+  <svg
+    class="rectangle-247"
+    width="243"
+    height="72"
+    viewBox="0 0 243 72"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g filter="url(#filter0_i_3_600)">
+      <path
+        d="M0 16C0 7.16344 7.16344 0 16 0H227C235.837 0 243 7.16344 243 16V56C243 64.8366 235.837 72 227 72H16C7.16345 72 0 64.8366 0 56V16Z"
+        fill="#03419E"
+        fill-opacity="0.05"
+      />
+    </g>
+    <defs>
+      <filter
+        id="filter0_i_3_600"
+        x="0"
+        y="0"
+        width="243"
+        height="76"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="BackgroundImageFix"
+          result="shape"
+        />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="4" />
+        <feGaussianBlur stdDeviation="2" />
+        <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+        />
+        <feBlend mode="normal" in2="shape" result="effect1_innerShadow_3_600" />
+      </filter>
+    </defs>
+  </svg>
+  <svg
+    class="rectangle-248"
+    width="243"
+    height="72"
+    viewBox="0 0 243 72"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g filter="url(#filter0_i_3_602)">
+      <path
+        d="M0 16C0 7.16344 7.16344 0 16 0H227C235.837 0 243 7.16344 243 16V56C243 64.8366 235.837 72 227 72H16C7.16345 72 0 64.8366 0 56V16Z"
+        fill="#03419E"
+        fill-opacity="0.05"
+      />
+    </g>
+    <defs>
+      <filter
+        id="filter0_i_3_602"
+        x="0"
+        y="0"
+        width="243"
+        height="76"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="BackgroundImageFix"
+          result="shape"
+        />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="4" />
+        <feGaussianBlur stdDeviation="2" />
+        <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+        />
+        <feBlend mode="normal" in2="shape" result="effect1_innerShadow_3_602" />
+      </filter>
+    </defs>
+  </svg>
+  <div class="_642-919">
+    642.919
+    <br />
+    <br />
+  </div>
+  <div class="jumlah-mahasiswa">Jumlah Mahasiswa</div>
+  <svg
+    class="rectangle-249"
+    width="243"
+    height="72"
+    viewBox="0 0 243 72"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g filter="url(#filter0_i_3_603)">
+      <path
+        d="M0 16C0 7.16344 7.16344 0 16 0H227C235.837 0 243 7.16344 243 16V56C243 64.8366 235.837 72 227 72H16C7.16345 72 0 64.8366 0 56V16Z"
+        fill="#03419E"
+        fill-opacity="0.05"
+      />
+    </g>
+    <defs>
+      <filter
+        id="filter0_i_3_603"
+        x="0"
+        y="0"
+        width="243"
+        height="76"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="BackgroundImageFix"
+          result="shape"
+        />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="4" />
+        <feGaussianBlur stdDeviation="2" />
+        <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+        />
+        <feBlend mode="normal" in2="shape" result="effect1_innerShadow_3_603" />
+      </filter>
+    </defs>
+  </svg>
+  <div class="_1-987">1.987</div>
+  <div class="jumlah-program-studi">Jumlah Program Studi</div>
+  <svg
+    class="rectangle-250"
+    width="243"
+    height="72"
+    viewBox="0 0 243 72"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g filter="url(#filter0_i_3_604)">
+      <path
+        d="M0 16C0 7.16344 7.16344 0 16 0H227C235.837 0 243 7.16344 243 16V56C243 64.8366 235.837 72 227 72H16C7.16345 72 0 64.8366 0 56V16Z"
+        fill="#03419E"
+        fill-opacity="0.05"
+      />
+    </g>
+    <defs>
+      <filter
+        id="filter0_i_3_604"
+        x="0"
+        y="0"
+        width="243"
+        height="76"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="BackgroundImageFix"
+          result="shape"
+        />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="4" />
+        <feGaussianBlur stdDeviation="2" />
+        <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+        />
+        <feBlend mode="normal" in2="shape" result="effect1_innerShadow_3_604" />
+      </filter>
+    </defs>
+  </svg>
+  <div class="_25-119">
+    25.119
+    <br />
+    <br />
+  </div>
+  <div class="jumlah-dosen-tetap">Jumlah Dosen Tetap</div>
+  <div class="_270">
+    270
+    <br />
+  </div>
+</div>
