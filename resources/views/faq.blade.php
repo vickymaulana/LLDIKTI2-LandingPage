aa8fc9f4c99e1034a279a7307954503ed4a57f62
vickymaulana <vickymaulanna@gmail.com>
2024-03-20 08:54:14 +0700

    faq update

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -186,61 +186,43 @@
 
 <div class="faq-dan-vid-yutup">
   <div class="group-36867">
-    <div class="accordion-item">
+    @php
+    $questions = [
+    ["Apa Itu LLDIKTI 2", "Apa itu LLDIKTI 2? LLDIKTI 2 adalah..."],
+    ["Apa itu Palembang", "Apa itu Palembang? Palembang adalah..."],
+    ["Apa itu Coding", "Apa itu Coding? Coding adalah..."],
+    ["Pinjam dulu seratus", "Pinjam dulu seratus adalah..."],
+    ["Apa itu Figma", "Apa itu Figma? Figma adalah..."],
+    ];
+    @endphp
+
+    @foreach($questions as $key => $question)
+    <div class="accordion-item{{$key + 1}}">
       <div class="divider"></div>
       <div class="title">
-        <div class="question">Apa Itu LLDIKTI 2</div>
-        <i class="fa-solid fa-caret-down" onclick="toggleDescription(1)"></i>
+        <div class="question">{{ $question[0] }}</div>
+        <i class="fa-solid fa-caret-down" onclick="toggleDescription({{ $key + 1 }})"></i>
       </div>
-      <div class="description" id="description1" style="display: none;">Apa itu LLDIKTI 2? LLDIKTI 2 adalah...</div>
+      <div class="description" id="description{{ $key + 1 }}" style="display: none;">{{ $question[1] }}</div>
     </div>
-    <div class="accordion-item2">
-      <div class="divider"></div>
-      <div class="title">
-        <div class="question">Apa itu Palembang</div>
-        <i class="fa-solid fa-caret-down" onclick="toggleDescription(2)"></i>
-      </div>
-      <div class="description" id="description2" style="display: none;">Apa itu Palembang? Palembang adalah...</div>
-    </div>
-    <div class="accordion-item3">
-      <div class="divider"></div>
-      <div class="title">
-        <div class="question">Apa itu Coding</div>
-        <i class="fa-solid fa-caret-down" onclick="toggleDescription(3)"></i>
-      </div>
-      <div class="description" id="description3" style="display: none;">Apa itu Coding? Coding adalah...</div>
-    </div>
-    <div class="accordion-item4">
-      <div class="divider"></div>
-      <div class="title">
-        <div class="question">Pinjam dulu seratus</div>
-        <i class="fa-solid fa-caret-down" onclick="toggleDescription(4)"></i>
-      </div>
-      <div class="description" id="description4" style="display: none;">Pinjam dulu seratus adalah...</div>
-    </div>
-    <div class="accordion-item5">
-      <div class="divider"></div>
-      <div class="title">
-        <div class="question">Apa itu Figma</div>
-        <i class="fa-solid fa-caret-down" onclick="toggleDescription(5)"></i>
-      </div>
-      <div class="description" id="description5" style="display: none;">Apa itu Figma? Figma adalah...</div>
-    </div>
+    @endforeach
+
     <div class="divider2"></div>
   </div>
   <div class="heading">FAQs</div>
   <div class="text">Pertanyaan Yang Sering Ditanyakan</div>
   <div data-aos="fade-right">
-  <iframe class="image-36" width="560" height="315" src="https://www.youtube.com/embed/V7zAO9J06hE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
-</div>
+    <iframe class="image-36" width="560" height="315" src="https://www.youtube.com/embed/V7zAO9J06hE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
+</div>
+</div>
 <script>
-  function toggleDescription(id) {
-    var description = document.getElementById("description" + id);
-    if (description.style.display === "none") {
-      description.style.display = "block";
-    } else {
-      description.style.display = "none";
-    }
+function toggleDescription(id) {
+  var description = document.getElementById("description" + id);
+  if (description.style.display === "none") {
+    description.style.display = "block";
+  } else {
+    description.style.display = "none";
   }
+}
 </script>
