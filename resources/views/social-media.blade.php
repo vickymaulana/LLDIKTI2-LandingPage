67f6f690f5eb76ad859749f50fcabd9ca7b8a41e
andresa11satriawan <andresa11satriawan@gmail.com>
2024-03-20 17:05:45 +0700

    update width social media

--- resources/views/social-media.blade.php
+++ resources/views/social-media.blade.php
@@ -1,5 +1,11 @@
-{{-- <style>
-    .social-media,
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .social-media,
 .social-media * {
   box-sizing: border-box;
 }
@@ -9,7 +15,12 @@
   height: 561px;
   position: relative;
   overflow: hidden;
+  background-color: aqua;
+
+  /* Menyesuaikan lebar dengan gambar */
+  width: 487px; /* (236px * 2) + 20px (jarak antar gambar) */
 }
+
 .group-36924 {
   position: absolute;
   inset: 0;
@@ -105,195 +116,43 @@
   object-fit: cover;
 }
 
-</style>
-<div class="social-media">
-  <img class="image-25" src="image-250.png" />
-  <img class="image-26" src="image-260.png" />
-  <svg
-    class="vector"
-    width="26"
-    height="27"
-    viewBox="0 0 26 27"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M12.7267 6.59054C9.15069 6.59054 6.26625 9.54717 6.26625 13.2127C6.26625 16.8782 9.15069 19.8349 12.7267 19.8349C16.3027 19.8349 19.1872 16.8782 19.1872 13.2127C19.1872 9.54717 16.3027 6.59054 12.7267 6.59054ZM12.7267 17.518C10.4158 17.518 8.52657 15.5872 8.52657 13.2127C8.52657 10.8382 10.4102 8.90744 12.7267 8.90744C15.0433 8.90744 16.9269 10.8382 16.9269 13.2127C16.9269 15.5872 15.0376 17.518 12.7267 17.518ZM20.9583 6.31966C20.9583 7.17841 20.2836 7.86426 19.4514 7.86426C18.6137 7.86426 17.9446 7.17265 17.9446 6.31966C17.9446 5.46668 18.6193 4.77507 19.4514 4.77507C20.2836 4.77507 20.9583 5.46668 20.9583 6.31966ZM25.2372 7.88731C25.1416 5.81825 24.6805 3.98548 23.2018 2.47547C21.7286 0.965455 19.9406 0.492855 17.9221 0.389114C15.8417 0.268082 9.60613 0.268082 7.52573 0.389114C5.51282 0.487092 3.7248 0.959691 2.24604 2.46971C0.767273 3.97972 0.311836 5.81248 0.210628 7.88155C0.0925512 10.014 0.0925512 16.4056 0.210628 18.5381C0.306213 20.6072 0.767273 22.4399 2.24604 23.9499C3.7248 25.46 5.50719 25.9326 7.52573 26.0363C9.60613 26.1573 15.8417 26.1573 17.9221 26.0363C19.9406 25.9383 21.7286 25.4657 23.2018 23.9499C24.6749 22.4399 25.136 20.6072 25.2372 18.5381C25.3553 16.4056 25.3553 10.0198 25.2372 7.88731ZM22.5495 20.8262C22.111 21.9558 21.2619 22.8261 20.1543 23.2814C18.4956 23.9557 14.5597 23.8001 12.7267 23.8001C10.8937 23.8001 6.95222 23.9499 5.29915 23.2814C4.19711 22.8318 3.34808 21.9616 2.90389 20.8262C2.24604 19.126 2.39785 15.0916 2.39785 13.2127C2.39785 11.3338 2.25166 7.29368 2.90389 5.59924C3.34246 4.46961 4.19148 3.59933 5.29915 3.14402C6.95784 2.4697 10.8937 2.62532 12.7267 2.62532C14.5597 2.62532 18.5012 2.47547 20.1543 3.14402C21.2563 3.59357 22.1053 4.46385 22.5495 5.59924C23.2074 7.29944 23.0556 11.3338 23.0556 13.2127C23.0556 15.0916 23.2074 19.1317 22.5495 20.8262Z"
-      fill="#03419E"
-    />
-  </svg>
-  <div class="lldiktii-2">@lldiktii2</div>
-  <svg
-    class="vector2"
-    width="31"
-    height="33"
-    viewBox="0 0 31 33"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M30.3582 16.2971C30.3582 7.39386 23.5886 0.178101 15.236 0.178101C6.88332 0.178101 0.11377 7.39386 0.11377 16.2971C0.11377 23.8528 4.99895 30.1997 11.5854 31.9438V21.2209H8.46642V16.2971H11.5854V14.1752C11.5854 8.69093 13.9128 6.14716 18.9693 6.14716C19.9262 6.14716 21.5802 6.34865 22.2595 6.55013V11.008C21.9051 10.9703 21.2848 10.9451 20.511 10.9451C18.03 10.9451 17.0731 11.9462 17.0731 14.5467V16.2971H22.0114L21.1608 21.2209H17.0672V32.2964C24.5574 31.3331 30.3582 24.5392 30.3582 16.2971Z"
-      fill="#03419E"
-    />
-  </svg>
-  <div class="lldikti-wilayah-2">LLDIKTI Wilayah 2</div>
-  <img class="image-27" src="image-270.png" />
-  <img class="image-28" src="image-280.png" />
-</div> --}}
-
-{{-- Di bawah ini adalah codingan Andre --}}
-<style>
-  .social-media,
-.social-media * {
-box-sizing: border-box;
-}
-.social-media {
-background: rgba(255, 255, 255, 0.2);
-border-radius: 20px;
-height: 561px;
-position: relative;
-overflow: hidden;
-}
-.group-36924 {
-position: absolute;
-inset: 0;
-}
-.image-25 {
-border-radius: 42px;
-border-style: solid;
-border-color: #03419f;
-border-width: 1px;
-width: 236px;
-height: 218px;
-position: absolute;
-left: 0px;
-top: 43px;
-object-fit: cover;
-}
-.image-26 {
-border-radius: 42px;
-border-style: solid;
-border-color: #03419f;
-border-width: 1px;
-width: 236px;
-height: 218px;
-position: absolute;
-left: 246px;
-top: 43px;
-object-fit: cover;
-}
-.vector {
-width: 5.09%;
-height: 4.6%;
-position: absolute;
-right: 91.85%;
-left: 3.05%;
-bottom: 93.74%;
-top: 1.66%;
-overflow: visible;
-}
-.lldiktii-2 {
-color: #03419e;
-text-align: left;
-font-family: "Roboto-ExtraBold", sans-serif;
-font-size: 18px;
-line-height: 150%;
-font-weight: 800;
-position: absolute;
-left: 45px;
-top: 7px;
-}
-.vector2 {
-width: 6.11%;
-height: 5.73%;
-position: absolute;
-right: 91.04%;
-left: 2.85%;
-bottom: 45.76%;
-top: 48.52%;
-overflow: visible;
-}
-.lldikti-wilayah-2 {
-color: #03419e;
-text-align: left;
-font-family: "Roboto-ExtraBold", sans-serif;
-font-size: 18px;
-line-height: 150%;
-font-weight: 800;
-position: absolute;
-left: 50px;
-top: 282px;
-}
-.image-27 {
-border-radius: 42px;
-border-style: solid;
-border-color: #03419f;
-border-width: 1px;
-width: 236px;
-height: 218px;
-position: absolute;
-left: 0px;
-top: 320px;
-object-fit: cover;
-}
-.image-28 {
-border-radius: 42px;
-border-style: solid;
-border-color: #03419f;
-border-width: 1px;
-width: 236px;
-height: 218px;
-position: absolute;
-left: 246px;
-top: 320px;
-object-fit: cover;
-}
-</style>
-
-<div style="position: relative; left: 900px;" class="social-media">
-  {{-- <img class="image-25" src="image-250.png" /> --}}
-  <a href="https://www.instagram.com/p/C4o2fiUPY5P/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="> <img class="image-25" src="{{ asset('img/1.jpg') }}" alt="Deskripsi Gambar"> </a>
-  {{-- <img class="image-26" src="image-260.png" /> --}}
-  <a href="https://www.instagram.com/p/C4iKMnfP7pd/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="> <img class="image-26" src="{{ asset('img/2.jpg') }}" alt="Deskripsi Gambar"> </a>
-  <a href="https://www.instagram.com/lldiktii2?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
-  <svg
-    class="vector"
-    width="26"
-    height="27"
-    viewBox="0 0 26 27"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    style="left: -10px;"
-  >
-    <path
-      d="M12.7267 6.59054C9.15069 6.59054 6.26625 9.54717 6.26625 13.2127C6.26625 16.8782 9.15069 19.8349 12.7267 19.8349C16.3027 19.8349 19.1872 16.8782 19.1872 13.2127C19.1872 9.54717 16.3027 6.59054 12.7267 6.59054ZM12.7267 17.518C10.4158 17.518 8.52657 15.5872 8.52657 13.2127C8.52657 10.8382 10.4102 8.90744 12.7267 8.90744C15.0433 8.90744 16.9269 10.8382 16.9269 13.2127C16.9269 15.5872 15.0376 17.518 12.7267 17.518ZM20.9583 6.31966C20.9583 7.17841 20.2836 7.86426 19.4514 7.86426C18.6137 7.86426 17.9446 7.17265 17.9446 6.31966C17.9446 5.46668 18.6193 4.77507 19.4514 4.77507C20.2836 4.77507 20.9583 5.46668 20.9583 6.31966ZM25.2372 7.88731C25.1416 5.81825 24.6805 3.98548 23.2018 2.47547C21.7286 0.965455 19.9406 0.492855 17.9221 0.389114C15.8417 0.268082 9.60613 0.268082 7.52573 0.389114C5.51282 0.487092 3.7248 0.959691 2.24604 2.46971C0.767273 3.97972 0.311836 5.81248 0.210628 7.88155C0.0925512 10.014 0.0925512 16.4056 0.210628 18.5381C0.306213 20.6072 0.767273 22.4399 2.24604 23.9499C3.7248 25.46 5.50719 25.9326 7.52573 26.0363C9.60613 26.1573 15.8417 26.1573 17.9221 26.0363C19.9406 25.9383 21.7286 25.4657 23.2018 23.9499C24.6749 22.4399 25.136 20.6072 25.2372 18.5381C25.3553 16.4056 25.3553 10.0198 25.2372 7.88731ZM22.5495 20.8262C22.111 21.9558 21.2619 22.8261 20.1543 23.2814C18.4956 23.9557 14.5597 23.8001 12.7267 23.8001C10.8937 23.8001 6.95222 23.9499 5.29915 23.2814C4.19711 22.8318 3.34808 21.9616 2.90389 20.8262C2.24604 19.126 2.39785 15.0916 2.39785 13.2127C2.39785 11.3338 2.25166 7.29368 2.90389 5.59924C3.34246 4.46961 4.19148 3.59933 5.29915 3.14402C6.95784 2.4697 10.8937 2.62532 12.7267 2.62532C14.5597 2.62532 18.5012 2.47547 20.1543 3.14402C21.2563 3.59357 22.1053 4.46385 22.5495 5.59924C23.2074 7.29944 23.0556 11.3338 23.0556 13.2127C23.0556 15.0916 23.2074 19.1317 22.5495 20.8262Z"
-      fill="#03419E"
-    />
-  </svg>
-  <div class="lldiktii-2">@lldiktii2</div>
-  </a>
-
-  {{-- tag a, link, dan i aku yg nambah --}}
-  {{-- <a href="https://www.instagram.com/lldiktii2?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="> <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <i class="fa fa-instagram" style="font-size:36px"></i>
-  <div class="lldiktii-2">@lldiktii2</div> </a> --}}
-  <a href="https://www.facebook.com/lldikti2"> <svg
-    class="vector2"
-    width="31"
-    height="33"
-    viewBox="0 0 31 33"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    style="left: -10px;"
-  >
-    <path
-      d="M30.3582 16.2971C30.3582 7.39386 23.5886 0.178101 15.236 0.178101C6.88332 0.178101 0.11377 7.39386 0.11377 16.2971C0.11377 23.8528 4.99895 30.1997 11.5854 31.9438V21.2209H8.46642V16.2971H11.5854V14.1752C11.5854 8.69093 13.9128 6.14716 18.9693 6.14716C19.9262 6.14716 21.5802 6.34865 22.2595 6.55013V11.008C21.9051 10.9703 21.2848 10.9451 20.511 10.9451C18.03 10.9451 17.0731 11.9462 17.0731 14.5467V16.2971H22.0114L21.1608 21.2209H17.0672V32.2964C24.5574 31.3331 30.3582 24.5392 30.3582 16.2971Z"
-      fill="#03419E"
-    />
-  </svg>
-  <div class="lldikti-wilayah-2">LLDIKTI Wilayah 2</div> </a>
-  {{-- <img class="image-27" src="image-270.png" /> --}}
-  <a href="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto.php%3Ffbid%3D953744876238712%26set%3Da.628566888756514%26type%3D3&show_text=true&width=500" width="500" height="582" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share>"> <img class="image-27" src="{{ asset('img/11.jpg') }}" alt="Deskripsi Gambar"> </a>
-  {{-- <img class="image-28" src="image-280.png" /> --}}
-  <a href="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto.php%3Ffbid%3D953742836238916%26set%3Da.628566888756514%26type%3D3&show_text=true&width=500" width="500" height="518" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"> <img class="image-28" src="{{ asset('img/22.jpg') }}" alt=""> </a>
-</div>
+    </style>
+</head>
+<body>
+    <div class="social-media">
+        <img class="image-25" src="image-250.png" />
+        <img class="image-26" src="image-260.png" />
+        <svg
+          class="vector"
+          width="26"
+          height="27"
+          viewBox="0 0 26 27"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M12.7267 6.59054C9.15069 6.59054 6.26625 9.54717 6.26625 13.2127C6.26625 16.8782 9.15069 19.8349 12.7267 19.8349C16.3027 19.8349 19.1872 16.8782 19.1872 13.2127C19.1872 9.54717 16.3027 6.59054 12.7267 6.59054ZM12.7267 17.518C10.4158 17.518 8.52657 15.5872 8.52657 13.2127C8.52657 10.8382 10.4102 8.90744 12.7267 8.90744C15.0433 8.90744 16.9269 10.8382 16.9269 13.2127C16.9269 15.5872 15.0376 17.518 12.7267 17.518ZM20.9583 6.31966C20.9583 7.17841 20.2836 7.86426 19.4514 7.86426C18.6137 7.86426 17.9446 7.17265 17.9446 6.31966C17.9446 5.46668 18.6193 4.77507 19.4514 4.77507C20.2836 4.77507 20.9583 5.46668 20.9583 6.31966ZM25.2372 7.88731C25.1416 5.81825 24.6805 3.98548 23.2018 2.47547C21.7286 0.965455 19.9406 0.492855 17.9221 0.389114C15.8417 0.268082 9.60613 0.268082 7.52573 0.389114C5.51282 0.487092 3.7248 0.959691 2.24604 2.46971C0.767273 3.97972 0.311836 5.81248 0.210628 7.88155C0.0925512 10.014 0.0925512 16.4056 0.210628 18.5381C0.306213 20.6072 0.767273 22.4399 2.24604 23.9499C3.7248 25.46 5.50719 25.9326 7.52573 26.0363C9.60613 26.1573 15.8417 26.1573 17.9221 26.0363C19.9406 25.9383 21.7286 25.4657 23.2018 23.9499C24.6749 22.4399 25.136 20.6072 25.2372 18.5381C25.3553 16.4056 25.3553 10.0198 25.2372 7.88731ZM22.5495 20.8262C22.111 21.9558 21.2619 22.8261 20.1543 23.2814C18.4956 23.9557 14.5597 23.8001 12.7267 23.8001C10.8937 23.8001 6.95222 23.9499 5.29915 23.2814C4.19711 22.8318 3.34808 21.9616 2.90389 20.8262C2.24604 19.126 2.39785 15.0916 2.39785 13.2127C2.39785 11.3338 2.25166 7.29368 2.90389 5.59924C3.34246 4.46961 4.19148 3.59933 5.29915 3.14402C6.95784 2.4697 10.8937 2.62532 12.7267 2.62532C14.5597 2.62532 18.5012 2.47547 20.1543 3.14402C21.2563 3.59357 22.1053 4.46385 22.5495 5.59924C23.2074 7.29944 23.0556 11.3338 23.0556 13.2127C23.0556 15.0916 23.2074 19.1317 22.5495 20.8262Z"
+            fill="#03419E"
+          />
+        </svg>
+        <div class="lldiktii-2">@lldiktii2</div>
+        <svg
+          class="vector2"
+          width="31"
+          height="33"
+          viewBox="0 0 31 33"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M30.3582 16.2971C30.3582 7.39386 23.5886 0.178101 15.236 0.178101C6.88332 0.178101 0.11377 7.39386 0.11377 16.2971C0.11377 23.8528 4.99895 30.1997 11.5854 31.9438V21.2209H8.46642V16.2971H11.5854V14.1752C11.5854 8.69093 13.9128 6.14716 18.9693 6.14716C19.9262 6.14716 21.5802 6.34865 22.2595 6.55013V11.008C21.9051 10.9703 21.2848 10.9451 20.511 10.9451C18.03 10.9451 17.0731 11.9462 17.0731 14.5467V16.2971H22.0114L21.1608 21.2209H17.0672V32.2964C24.5574 31.3331 30.3582 24.5392 30.3582 16.2971Z"
+            fill="#03419E"
+          />
+        </svg>
+        <div class="lldikti-wilayah-2">LLDIKTI Wilayah 2</div>
+        <img class="image-27" src="image-270.png" />
+        <img class="image-28" src="image-280.png" />
+      </div>
+      
+</body>
+</html>
